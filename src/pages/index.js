ea27b1c54f3f0cc9c33def8871fda979e6367dfe
sw-yx <shawnthe1@gmail.com>
2018-04-02 23:04:02 -0400

    add youtube

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,20 @@ const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to the new Firebug site. Here is our first video.</p>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/wZaFOfoCxjY?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/wZaFOfoCxjY?rel=0"
+      allowFullScreen="allowfullscreen"
+      frameBorder="0"
+      allow="autoplay; encrypted-media"
+    />
+    <p>
+      Follow us on Twitter{' '}
+      <a href="https://twitter.com/firebugfam" target="_blank">
+        @FireBugFam
+      </a>
+    </p>
     <p>Now go build something great, with Firebase!</p>
   </div>
 )
