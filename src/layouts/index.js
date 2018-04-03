124992a9a80f05ed9930d0651ab20b5ebb98173d
sw-yx <shawnthe1@gmail.com>
2018-04-02 20:51:11 -0400

    init

---
+++ src/layouts/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+
+import Header from '../components/Header'
+import './index.css'
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="🔥 🐛 Firebug"
+      meta={[
+        {
+          name: 'description',
+          content: 'firebase user group: tutorials and walkthroughs',
+        },
+        { name: 'keywords', content: 'firebase, firestore, firebase tutorial' },
+      ]}
+    />
+    <Header />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children()}
+    </div>
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Firebug site.</p>
+    <p>Now go build something great, with Firebase!</p>
+  </div>
+)
+
+export default IndexPage
