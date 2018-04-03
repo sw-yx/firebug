45d758289b93607f54caf3d296ab63c00da61112
sw-yx <shawnthe1@gmail.com>
2018-04-02 23:00:27 -0400

    add new vid

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Firebug site.</p>
+    <p>Welcome to the new Firebug site. Here is our first video.</p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/wZaFOfoCxjY?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     <p>Now go build something great, with Firebase!</p>
   </div>
 )
